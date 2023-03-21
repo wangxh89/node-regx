c099e2e7f65b7c8df680e83725fa5fe20735506c
wang.xiaohu9 <wang.xiaohu9@iwhalecloud.com>
2023-03-21 15:37:18 +0800

    sec comit

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ fs.readFile('teste.txt', 'utf8', (err, data) => {
   let replacedData = data.replace(/<a name='(.*?)'><\/a>/g, "<a name=\"$1\"></a>");
 
   // 将修改后的内容写回到文件中
-  fs.writeFile('file.html', replacedData, 'utf8', (err) => {
+  fs.writeFile('teste.txt', replacedData, 'utf8', (err) => {
     if (err) throw err;
     console.log('文件内容替换成功！');
   });
